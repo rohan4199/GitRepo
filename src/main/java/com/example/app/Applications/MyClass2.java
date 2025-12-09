91e64bda21fe72c5ac447a2cb268df0982f64790
rohan4199 <66842933+rohan4199@users.noreply.github.com>
2025-12-09 18:58:58 +0530

    Add files via upload

---
+++ src/main/java/com/example/app/Applications/MyClass2.java
@@ -0,0 +1,11 @@
+public class MyClass2 {
+  static void myMethod() {
+    System.out.println("Hello World!");
+  }
+
+  public static void main(String[] args) {
+    myMethod();
+  }
+}
+
+// Outputs "Hello World!"
